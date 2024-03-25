de23c330c59104a9742d97f4a0d95439ccfe784a
derej_gnow <jeredwong711@gmail.com>
2024-03-26 01:18:19 +0800

    Player Border and Reposition Player Name (Unfinished)

---
+++ src/gui/RoundedBorder.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class RoundedBorder implements Border {
+    private int radius;
+    private Color color;
+    private int thickness; // Border thickness
+
+    public RoundedBorder(int radius, Color color, int thickness) {
+        this.radius = radius;
+        this.color = color;
+        this.thickness = thickness;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setColor(color);
+        g2d.setStroke(new BasicStroke(thickness)); // Set the stroke width
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        g2d.dispose();
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius + thickness, radius + thickness, radius + thickness, radius + thickness);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+}
