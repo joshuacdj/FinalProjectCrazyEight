2dc7b444dd4d6fb58ac43fa6805f3c33a7f121c6
jinjunchia <144515009+jinjunchia@users.noreply.github.com>
2024-03-27 14:53:12 +0800

    audio

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -123,6 +123,7 @@ public class Controller implements DrawActionListener{
 
     private void startGame() {
         welcomeClickSound();
+        stopSound("welcomeSound");
         showScreen(inGameScreen);
     }
     
