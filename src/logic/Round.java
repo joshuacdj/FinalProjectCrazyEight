710e034fcc7a1f68c7593c5c7d36c70ddc39a279
eohnai <117977929+eohnai@users.noreply.github.com>
2024-03-27 17:15:53 +0800

    Update round comments

--- src/logic/Round.java
+++ src/logic/Round.java
@@ -9,12 +9,19 @@ public class Round {
     private DiscardPile discardPile;
 
     public Round() {
+        // Creating a new array list to store the players of the game
         listOfPlayers = new ArrayList<>();
+
+        // Creating the 4 different players
         listOfPlayers.add(new Human("You"));
         listOfPlayers.add(new Computer("Comp 1"));
         listOfPlayers.add(new Computer("Comp 2"));
         listOfPlayers.add(new Computer("Comp 3"));
+
+        // Creating draw pile
         this.drawPile = new DrawPile();
+
+        // Creating discard pile
         this.discardPile = new DiscardPile();
     }
 
@@ -26,34 +33,32 @@ public class Round {
         return discardPile;
     }
 
-
     public ArrayList<Player> getListOfPlayers() {
         return listOfPlayers;
     }
 
-
-    // set the first card of the round
     public void setFirstCard(DrawPile drawPile, DiscardPile discardPile) {
-        // obtain starting card
+        // Obtain starting card
         Card startingCard = drawPile.getTopCard();
 
-        // if starting card drawn is 8, put it back into the pile and draw a new starting card
+        // If starting card drawn is 8, put it back into the pile and draw a new starting card
         while (startingCard.getValue() == 8) {
             drawPile.getListOfCards().addFirst(startingCard);
             startingCard = drawPile.getTopCard();
         }
 
-        // add starting card to discard pile
+        // Add starting card to discard pile
         discardPile.addCard(startingCard);
     }
 
     public void roundStart() {
-        // shuffle a new deck
+        // Shuffle a new deck
         drawPile.shuffleDeck();
 
-        // set the first playing card of the game
+        // Set the first playing card of the game
         setFirstCard(drawPile, discardPile);
 
+        // Give out 5 cards from the draw pile to each of the 4 players, one at a time
         for (int i = 0; i < STARTINGDRAWCOUNT; i++) {
             for (Player p : listOfPlayers) {
                 p.drawCard(drawPile.getTopCard());
@@ -61,4 +66,3 @@ public class Round {
         }
     }
 }
-
